cd6b8dedd3e8f8d4becba0f198c77db208469a8c
Mighty/ <realofficialvictor@gmail.com>
2025-11-08 03:44:42 +0100

    Uncomment Header and Footer components in Portal

--- src/Pages/E-Portal/Portal.jsx
+++ src/Pages/E-Portal/Portal.jsx
@@ -1,5 +1,5 @@
-// import { Header } from '../../Components/Header'
-// import { Footer } from '../../Components/Footer'
+import { Header } from '../../Components/Header'
+import { Footer } from '../../Components/Footer'
 
 
 
@@ -7,9 +7,9 @@ export function Portal() {
   return (
     <>
     <title>SGIC | School E-Portal</title>
-      {/* <Header /> */}
+      <Header /> 
       <iframe src='//schoolviewers.com/3275/portal' style='width:100%;height:560px;border:1px solid #bbb;border-radius:3px;'></iframe> 
-      {/* <Footer /> */}
+      <Footer /> 
     </>
   )
 }
